13d73c37179aa10248796465bdd39cd0b48fac67
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2022-11-01 22:04:34 +0200

    feat: implement city, token and language options

--- weather.js
+++ weather.js
@@ -1,6 +1,46 @@
 #!/usr/bin/env node
 import { parseArgs } from './helpers/args.js';
-import { logHelp } from './services/log.service.js';
+import { logSuccess, logError, logHelp } from './services/log.service.js';
+import { saveKeyValue, STORAGE_DICT } from './services/storage.service.js';
+
+async function saveCity(city) {
+  if (typeof city !== 'string') {
+    return logError("City wasn't specified");
+  }
+
+  try {
+    await saveKeyValue(STORAGE_DICT.city, city);
+    logSuccess('City saved');
+  } catch (error) {
+    logError(error.message);
+  }
+}
+
+async function saveToken(token) {
+  if (typeof token !== 'string') {
+    return logError("Token wasn't specified");
+  }
+
+  try {
+    await saveKeyValue(STORAGE_DICT.token, token);
+    logSuccess('Token saved');
+  } catch (error) {
+    logError(error.message);
+  }
+}
+
+async function saveLang(lang) {
+  if (typeof lang !== 'string') {
+    return logError("Language wasn't specified");
+  }
+
+  try {
+    await saveKeyValue(STORAGE_DICT.lang, lang);
+    logSuccess('Language saved');
+  } catch (error) {
+    logError(error.message);
+  }
+}
 
 function initCLI() {
   const args = process.argv.slice(2);
@@ -8,6 +48,18 @@ function initCLI() {
 
   if (opts.h) {
     return logHelp();
+  }
+
+  if (opts.c) {
+    saveCity(opts.c);
+  }
+
+  if (opts.t) {
+    saveToken(opts.t);
+  }
+
+  if (opts.l) {
+    saveLang(opts.l);
   }
 }
 
